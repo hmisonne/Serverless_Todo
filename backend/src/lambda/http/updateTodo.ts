74a43dfb7ebf49083618c487eee55db93eeb7197
hmisonne <helene.misonne@gmail.com>
2020-06-01 14:41:28 -0700

    Updated to double quotes and removed expression att names

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -7,7 +7,7 @@ import * as AWS from 'aws-sdk'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 const todoTable = process.env.TODOS_TABLE
-const todoIdIndex = process.env.TODO_ID_INDEX
+// const todoIdIndex = process.env.TODO_ID_INDEX
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
@@ -19,15 +19,15 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
 
   const userId = parseUserId(jwtToken)
 
-  if (!(await getTodo(todoId))) {
-     return {
-          statusCode: 404,
-          headers: {
-            'Access-Control-Allow-Origin': '*'
-          },
-          body: 'Todo Id does not exist'
-        };
-  }
+  // if (!(await getTodo(todoId))) {
+  //    return {
+  //         statusCode: 404,
+  //         headers: {
+  //           'Access-Control-Allow-Origin': '*'
+  //         },
+  //         body: 'Todo Id does not exist'
+  //       };
+  // }
   const items = await updateTodo(todoId, userId, updatedTodo)
   return {
     statusCode: 200,
@@ -47,16 +47,12 @@ async function updateTodo(todoId: string, userId: string, updatedTodo: UpdateTod
       Key: { 
         todoId, 
         userId },
-      UpdateExpression: 'set #NAME = :name, #DUEDATE :dueDate, #DONE :done',
-      ExpressionAttributeNames: {
-        '#NAME' : 'name',
-        '#DUEDATE' : 'dueDate',
-        '#DONE' : 'done'
-      },
+      UpdateExpression: "set #N = :name, dueDate :dueDate, done :done",
+      ExpressionAttributeNames: {"#N": "name"},
       ExpressionAttributeValues: {
-        ':name' : updatedTodo.name,
-        ':dueDate' : updatedTodo.dueDate,
-        ':done' : updatedTodo.done,
+        ":name": updatedTodo.name,
+        ":dueDate": updatedTodo.dueDate,
+        ":done": updatedTodo.done,
       },
       ReturnValues: "UPDATED_NEW"
     })
@@ -65,15 +61,15 @@ async function updateTodo(todoId: string, userId: string, updatedTodo: UpdateTod
   return newItem 
 }
 
-async function getTodo(todoId: string){
-  const result = await docClient.query({
-    TableName: todoTable,
-    IndexName: todoIdIndex,
-    KeyConditionExpression: 'todoId= :todoId',
-    ExpressionAttributeValues: {
-      'todoId': todoId
-    }
-  }).promise()
+// async function getTodo(todoId: string){
+//   const result = await docClient.query({
+//     TableName: todoTable,
+//     IndexName: todoIdIndex,
+//     KeyConditionExpression: 'todoId= :todoId',
+//     ExpressionAttributeValues: {
+//       'todoId': todoId
+//     }
+//   }).promise()
 
-  return result.Items[0]
-}
+//   return result.Items[0]
+// }
