f5656f510b53cc75450d442794778e6fd439fd4d
hmisonne <helene.misonne@gmail.com>
2020-06-01 09:18:43 -0700

    Updated Auth

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,9 +1,9 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
-// import { verify, decode } from 'jsonwebtoken'
+import { verify, decode } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
-// import Axios from 'axios'
+import Axios from 'axios'
 // import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
@@ -13,6 +13,8 @@ const logger = createLogger('auth')
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
 // const jwksUrl = '...'
+
+const jwksUrl = 'https://fsnd-hm.auth0.com/.well-known/jwks.json'
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -61,9 +63,16 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  if (token !== '123')
-    throw new Error('Invalid token')
-  // A request has been authorized
+  // get certificate from test endpoint
+  let cert: string;
+  try {
+    const response = await Axios.get(jwksUrl);
+    const pemData = response['data']['keys'][0]['x5c'][0];
+    cert = `-----BEGIN CERTIFICATE-----\n${pemData}\n-----END CERTIFICATE-----`;
+  } catch (err) {
+    console.log(err);
+  }
+  return verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload
 }
 
 function getToken(authHeader: string): string {
@@ -77,3 +86,4 @@ function getToken(authHeader: string): string {
 
   return token
 }
+
