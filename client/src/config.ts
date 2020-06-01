5f1ec2260a94eb04f62a6bbca77ea3f9374a7d19
hmisonne <helene.misonne@gmail.com>
2020-05-31 19:33:15 -0700

    Added config Auth0

--- client/src/config.ts
+++ client/src/config.ts
@@ -4,7 +4,7 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'fsnd-hm.auth0.com',            // Auth0 domain
+  clientId: 'NeVPTKmxhvQSaAyFQuOGfsjNutXBDDTg',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
