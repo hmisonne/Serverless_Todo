87c9ed9288064f6726b0b35f15e8531ec719e597
hmisonne <helene.misonne@gmail.com>
2020-06-02 07:25:00 -0700

    Added KeyConditionExpression

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -3,6 +3,7 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 import { createLogger } from '../../utils/logger'
 import * as AWS from "aws-sdk"
+import { getUserId } from '../utils'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 const todosTable = process.env.TODOS_TABLE
@@ -12,8 +13,13 @@ const logger = createLogger('getTodos')
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
   logger.info('Processing event: ', event)
+  const userId = getUserId(event)
   const result = await docClient.query({
-    TableName: todosTable
+    TableName: todosTable,
+    KeyConditionExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+        ':userId': userId
+    }
   }).promise()
   
   const items = result.Items
